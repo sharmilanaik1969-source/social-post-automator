3d2eb1362d083f70ec1a2d0360230f5409954ac0
sharmilanaik1969-source <sharmilanaik1969@gmail.com>
2025-11-17 16:08:01 +0530

    Add Header component with basic styling

---
+++ components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header: React.FC = () => {
+  return (
+    <header style={{ padding: '20px', backgroundColor: '#f0f0f0' }}>
+      <h1>My Website Header</h1>
+    </header>
+  );
+};
+
+export default Header;
